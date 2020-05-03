efb57e4ba42e242005e9d1feacc75f40f9134534
UPSJhona <34308770+UPSJhona@users.noreply.github.com>
2020-05-02 19:22:05 -0500

    Telefono login Interfaz

---
+++ Practica2.2/src/ups/edu/ec/modelo/Telefono.java
@@ -0,0 +1,81 @@
+package ups.edu.ec.modelo;
+
+public class Telefono {
+	private int codigo;
+	private String numero;
+	private String tipo;
+	private String operadora;
+	
+	
+	
+	
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+
+	public String getNumero() {
+		return numero;
+	}
+
+
+
+
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+
+
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+
+
+	public String getOperadora() {
+		return operadora;
+	}
+
+
+
+
+
+	public void setOperadora(String operadora) {
+		this.operadora = operadora;
+	}
+
+
+
+
+
+	public Telefono() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
